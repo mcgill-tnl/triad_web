ea0a5654f7ca512a9d1fb6bbbd31930713088d6e
sulantha <sulantha.s@gmail.com>
2019-04-02 14:54:28 -0400

    Added Description Page

--- assets/js/custom/smooth_scroll.js
+++ assets/js/custom/smooth_scroll.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.nav-link, .navbar-brand, .new-button').click(function() {
+  $('.nav-link, .navbar-brand, .new-button, .sidebar-link').click(function() {
       var sectionTo = $(this).attr('href');
       $('html, body').animate({
         scrollTop: $(sectionTo).offset().top -100
